33144da6f9c51ac3bc79d53e2db740502207718f
GrepSuzette <emugel@qq.com>
2019-06-23 19:00:51 +0800

    Letting non-canonical termios work well with ansi+cr

--- linc/linc_sys.cpp
+++ linc/linc_sys.cpp
@@ -79,7 +79,7 @@ namespace linc {
             bool is_canonical = true;               // we sit in  canonical by default
             bool has_atexit_been_set = false;   
             struct termios orig_termios;            // default, canonical
-            struct termios new_termios;             // non-canonical
+            struct termios terminal_settings;             // non-canonical
 
             // this needs be called before exiting
             // to restore canonical mode (i.e. waiting ENTER key)
@@ -94,13 +94,68 @@ namespace linc {
                     tcgetattr(0, &orig_termios);
                 }
                 if (is_canonical) {
-                    memcpy(&new_termios, &orig_termios, sizeof(new_termios));
+                    memcpy(&terminal_settings, &orig_termios, sizeof(terminal_settings));
                     if (!has_atexit_been_set) {
                         atexit(reset_terminal_mode);
                         has_atexit_been_set = true;
                     }
-                    cfmakeraw(&new_termios);
-                    tcsetattr(0, TCSANOW, &new_termios);
+                    cfmakeraw(&terminal_settings);
+                    // From man tcsetattr()
+                    // ONLCR  (XSI) Map NL to CR-NL on output.
+                    // OCRNL  Map CR to NL on output.
+                    // ONOCR  Don't output CR at column 0.
+                    // ONLRET Don't output CR.
+
+                    /* Let BREAK cause a SIGINT in input. */
+                    terminal_settings.c_iflag &= ~IGNBRK;
+                    terminal_settings.c_iflag |=  BRKINT;
+
+                    /* Ignore framing and parity errors in input. */
+                    terminal_settings.c_iflag |=  IGNPAR;
+                    terminal_settings.c_iflag &= ~PARMRK;
+
+                    /* Do not strip eighth bit on input. */
+                    terminal_settings.c_iflag &= ~ISTRIP;
+
+                    /* Do not do newline translation on input. */
+                    terminal_settings.c_iflag &= ~(INLCR | IGNCR | ICRNL);
+                    #ifdef IUCLC
+                    /* Do not do uppercase-to-lowercase mapping on input. */
+                    terminal_settings.c_iflag &= ~IUCLC;
+                    #endif
+
+                    /* Use 8-bit characters. This too may affect standard streams,
+                     * but any sane C library can deal with 8-bit characters. */
+                    terminal_settings.c_cflag &= ~CSIZE;
+                    terminal_settings.c_cflag |=  CS8;
+
+                    /* Enable receiver. */
+                    terminal_settings.c_cflag |=  CREAD;
+
+                    /* Let INTR/QUIT/SUSP/DSUSP generate the corresponding signals. */
+                    terminal_settings.c_lflag |=  ISIG;
+
+                    /* Enable noncanonical mode.
+                     * This is the most important bit, as it disables line buffering etc. */
+                    terminal_settings.c_lflag &= ~ICANON;
+
+                    /* Disable echoing input characters. */
+                    terminal_settings.c_lflag &= ~(ECHO | ECHOE | ECHOK | ECHONL);
+
+                    /* Disable implementation-defined input processing. */
+                    terminal_settings.c_lflag &= ~IEXTEN;
+
+                    /* Output flags */
+                    terminal_settings.c_oflag |= OPOST;        // ENABLE implementation-defined output processing
+                                                               // otherwise ansi and newline we work poorly
+
+                    /* To maintain best compatibility with normal behaviour of terminals,
+                     * we set TIME=0 and MAX=1 in noncanonical mode. This means that
+                     * read() will block until at least one byte is available. */
+                //    terminal_settings.c_cc[VTIME] = 0;
+                 //   terminal_settings.c_cc[VMIN] = 1;
+                    tcsetattr(0, TCSANOW, &terminal_settings);
+
                     is_canonical = false;
                 }
             }
